0cd8b7cfa94b35e385c7b38cc2960498734912e5
MioNok <nokelainen.m@gmail.com>
2019-04-14 20:21:38 +0300

    More progress

--- launchAlgo.py
+++ launchAlgo.py
@@ -1,110 +1,55 @@
 #When the database is populated we can analyse the stocks and send trade orders.
+#The script also fetches new data on a daily basis
 
 import alpaca_trade_api as tradeapi
 import pandas as pd
 import time
-import talib
+import sqlalchemy
 
-NY = 'America/New_York'
+from populate_database import read_data_daily
+from populate_database import write_data_to_sql
+from populate_database import read_snp_tickers
+
+#Database details
+from populate_database import serverSite
+
+
+#Incoming..
+#import talib
+
+#Alpaca tradeApi
 api = tradeapi.REST(
-    key_id='InsertKey',
-    secret_key='InserKey',
-    base_url='https://paper-api.alpaca.markets'
+    key_id="Api_key",
+    secret_key="Secret_key",
+    base_url="https://paper-api.alpaca.markets"
 )
 
+sleepBetweenCalls = 10
 
-
-#This is from the alpaca site..work in progress.
-def _get_prices(symbols, end_dt, max_workers=5):
-    '''Get the map of DataFrame price data from Alpaca's data API.'''
-
-    start_dt = end_dt - pd.Timedelta('50 days')
-    start = start_dt.strftime('%Y-%m-%d')
-    end = end_dt.strftime('%Y-%m-%d')
-
-    def get_barset(symbols):
-        return api.get_barset(
-            symbols,
-            'day',
-            limit = 50,
-            start=start,
-            end=end
-        )
-
-    # The maximum number of symbols we can request at once is 200.
-    barset = None
-    idx = 0
-    while idx <= len(symbols) - 1:
-        if barset is None:
-            barset = get_barset(symbols[idx:idx+200])
-        else:
-            barset.update(get_barset(symbols[idx:idx+200]))
-        idx += 200
-
-    return barset.df
-
-def prices(symbols):
-    '''Get the map of prices in DataFrame with the symbol name key.'''
-    now = pd.Timestamp.now(tz=NY)
-    end_dt = now
-    if now.time() >= pd.Timestamp('09:30', tz=NY).time():
-        end_dt = now - \
-            pd.Timedelta(now.strftime('%H:%M:%S')) - pd.Timedelta('1 minute')
-    return _get_prices(symbols, end_dt)
-
-
-
-
-
-
-def getLatestData():
-    def read_data_daily_and_ti(tickers = dowTickers):
-    rawStockDataDaily = pd.DataFrame()
-
-    for ticker in tickers:
-        retry = True
-        retry_counter = 0
-        
-        while retry:
-            tempRawStockDataDaily = pd.read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED"+
-                                                "&symbol="+ ticker +
-                                                "&outputsize=compact"+
-                                                "&datatype=csv"+
-                                                "&apikey="+ apikey[key_counter])
-            
-            #Check that the data is correct
-            if tempRawStockDataDaily.shape[1] == 9:
-                 #Write the ticker name to the df to keep track of what data belong where.
-                tempRawStockDataDaily["ticker"] = ticker
-                print("Fetched " + ticker + " daily data" )
-                keyCounter()
-                
-                #Saving the reults for this ticker and moving on to the following ticker.
-                rawStockDataDaily = rawStockDataDaily.append(tempRawStockDataDaily)
-                
-                #Free API key only gets you so far, as of writing this alphavantage is limiting the amount of API calls you can make in a minute..
-                time.sleep(sleeptime)
-                retry = False
-                
-            else: 
-                #Fetch has failed. Think about what you have done and try again.
-                retry_counter += 1 # counting the retrys, if above 10, stop pining the server, its not happening.
-                print("Retrying to fetch ", ticker)
-                
-                if retry_counter == 5:
-                    print("The fetch has failed 5 times in a row, something is wrong with the server, your api call or your key. Jumping over this one.")
-                    retry = False
-                #Lets try again.    
-                else: 
-                    retry = True
-                time.sleep(sleeptime)
-            
-      
-    return rawStockDataDaily
+def getLastData():
+    query = """SELECT timestamp FROM dailydata ORDER BY timestamp  DESC limit 1;"""
+    engine = sqlalchemy.create_engine(serverSite)
+    lastData = pd.read_sql(query, engine)
+    return lastData.iloc[0,0]
+    
 
 def main():
     
-    while True:
+    while True: 
         
-        price_df = prices(["AAPL"])
-        applsma = talib.SMA(price_df.iloc(3),200)
+        clock = api.get_clock()
+        now = clock.timestamp
+        
+        if api.get_clock == False and "12:00" in str(now):
+            #Read the latest data saved in the database:
+            lastData = getLastData()
+            #Read the latest possible data to get. Just testing with aapl, any ticker will do.
+            latestData = read_data_daily(tickers = ["AAPL"], outputsize = "compact").iloc[0:0]
+                        
+            #If the lastdata and latestdata is not the same and the time is 12:00 and market is closed we can search for new data!
+            if lastData != latestData:  
+                tickers = read_snp_tickers()
+                newStockData = read_data_daily(tickers, outputsize = "compact", saveLatestOnly = True)
+                write_data_to_sql(newStockData, "dailydata", if_exists = "append")
+                
+    time.sleep(sleepBetweenCalls)
--- populate_database.py
+++ populate_database.py
@@ -1,5 +1,6 @@
 ###
 #This script will initially populate the database with the stock data from alphavantage and the tickers and fisc data from datahub.
+#Some of the fuctions are used in the other scripts
 ###
 
 import pandas as pd
@@ -7,13 +8,17 @@ import sqlalchemy
 import time
 
 
-#This works when assuming you have a root user and the host is localhost.
+#This works when assuming host is localhost.
 #Later version will use a cloud provider like AWS.
 
 #Edit these
-apikey = ["InsertApiKey"] # Insert apikey.
-serverpass = "defaultpass" #isert your mysql server 
-database = "stockdata" #database in your mysql you want to use.
+apikey = ["insertApikey"] # Insert Alphavantage apikey.
+serverpass = "defaultpass" #insert your mysql serverpassword
+serveruser = "root" #insert your mysql serverpassword
+database = "stockdata" #database in your mysql you want to use. Need to be setup before running (Create DATABASE DatabaseName)
+serverSite = "mysql+pymysql://"+serveruser+":"+serverpass+"@localhost:3306/"+database
+
+#How long to sleep between search cycles
 sleeptime = 5
 
 key_counter = 0
@@ -31,7 +36,7 @@ dowTickers = ["AXP","AAPL","BA","CAT","CSCO","CVX","DIS","DOW","GS",
               "WMT","XOM"]
 
 #Uses the dow tickers if nothing else is specified.
-def read_data_daily_and_ti(tickers = dowTickers):
+def read_data_daily(tickers = dowTickers, outputsize = "full", saveLatestOnly = False):
     rawStockDataDaily = pd.DataFrame()
 
     for ticker in tickers:
@@ -41,7 +46,7 @@ def read_data_daily_and_ti(tickers = dowTickers):
         while retry:
             tempRawStockDataDaily = pd.read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED"+
                                                 "&symbol="+ ticker +
-                                                "&outputsize=full"+
+                                                "&outputsize="+outputsize+
                                                 "&datatype=csv"+
                                                 "&apikey="+ apikey[key_counter])
             
@@ -53,7 +58,10 @@ def read_data_daily_and_ti(tickers = dowTickers):
                 keyCounter()
                 
                 #Saving the reults for this ticker and moving on to the following ticker.
-                rawStockDataDaily = rawStockDataDaily.append(tempRawStockDataDaily)
+                if saveLatestOnly:
+                    rawStockDataDaily = rawStockDataDaily.append(tempRawStockDataDaily.iloc[0:])
+                else:
+                    rawStockDataDaily = rawStockDataDaily.append(tempRawStockDataDaily)
                 
                 #Free API key only gets you so far, as of writing this alphavantage is limiting the amount of API calls you can make in a minute..
                 time.sleep(sleeptime)
@@ -72,7 +80,6 @@ def read_data_daily_and_ti(tickers = dowTickers):
                     retry = True
                 time.sleep(sleeptime)
             
-      
     return rawStockDataDaily
 
 
@@ -80,7 +87,7 @@ def read_data_daily_and_ti(tickers = dowTickers):
 #Depending on if you are fetcing the data all at once or not appending or replaing the data might be the right option.
 def write_data_to_sql(dailyStockData, table_name, if_exists = "replace"):
     #you need mysql alchemy and pymysql to run this. syntax is:  Username:password@host:port/database
-    engine = sqlalchemy.create_engine("mysql+pymysql://root:"+serverpass+"@localhost:3306/stockdata")
+    engine = sqlalchemy.create_engine(serverSite)
     #Writing the data, name is table name. 
     dailyStockData.to_sql(name = table_name, con = engine,index = False,  if_exists = if_exists)
 
@@ -89,7 +96,7 @@ def read_snp_tickers():
             FROM fiscdata;"""
 
     #Creating the sqlalchemy engine and read sample data.
-    engine = sqlalchemy.create_engine("mysql+pymysql://root:"+serverpass+"@localhost:3306/stockdata")
+    engine = sqlalchemy.create_engine(serverSite)
     tickers = pd.read_sql(query, engine)
     return tickers
 
@@ -103,15 +110,16 @@ def keyCounter():
     
 #Main
 def main():
-    
+
     fiscStockData = getSnP500data() 
     write_data_to_sql(fiscStockData, "fiscdata", if_exists = "replace")    
     snpTickers = read_snp_tickers()
     
     #Do not currently want to wait it to fetch all 500 stocks so subsetting the amout for now.
-    stockdata = read_data_daily_and_ti(snpTickers["Symbol"].tolist())   
+    stockdata = read_data_daily(snpTickers["Symbol"][0:2].tolist())   
     write_data_to_sql(stockdata, "dailydata")
 
 
-main()
+if __name__ == "__main__":
+    main()
 
